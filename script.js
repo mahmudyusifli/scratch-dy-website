e63e51c30c7179a01e1801cc27efcd9315b3cdf4
Mahmud <60064090+mahmudyusifli@users.noreply.github.com>
2024-04-01 17:23:23 +0200

    Update script.js
    added function

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const progressBar = document.getElementById("progress-bar");
+  const progressBarContainer = document.getElementById(
+    "progress-bar-container"
+  );
+
+  window.addEventListener("scroll", function () {
+    const windowHeight = window.innerHeight;
+    const fullHeight = document.body.clientHeight;
+    const scrolled = window.scrollY;
+
+    const progress = (scrolled / (fullHeight - windowHeight)) * 100;
+    progressBar.style.width = progress + "%";
+  });
+});
